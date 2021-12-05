ff691233c0da001285cab6209abec2bb9e3dfff7
Магди Османов <magdi.osmanov97@gmail.com>
2021-12-05 16:47:14 +0300

    Добавьте файлы проекта.

---
+++ Ex.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            double S, V;
+            Squar(a, out S,out V);
+            Console.WriteLine(S);
+            Console.WriteLine(V);
+            Console.ReadKey();
+        }
+        static void Squar(int a,  out double S,out double V)
+        {
+            S = 6 * a * a;
+            V = a * a * a;
+
+        }
+    }
+}
---
+++ H.W. 7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H.W._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            int b1 = Convert.ToInt32(Console.ReadLine());
+            int c1 = Convert.ToInt32(Console.ReadLine());
+            double S, S1, Max;
+            Squar(a, b, c, out S);
+            Squar(a1, b1, c1, out S1);
+            Max = Math.Max(S1, S);
+            Console.WriteLine(S);
+            Console.WriteLine(S1);
+            Console.WriteLine(Max);
+            Console.ReadKey();
+        }
+        static void Squar(int a,int b,int c, out double S)
+        {
+            double p = (a + b + c) / 2;
+
+            S = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+    }
+}
